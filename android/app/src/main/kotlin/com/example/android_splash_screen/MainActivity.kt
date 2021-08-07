02c1d17941b3d0bb1a85d9a0f226917a9ab00053
mona-apk <cordion_nap@yahoo.co.jp>
2021-08-07 21:56:15 +0900

    feat: SplashScreenの設定

--- android/app/src/main/kotlin/com/example/android_splash_screen/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/android_splash_screen/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android_splash_screen
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): SplashScreen? = CustomSplashScreen()
 }
